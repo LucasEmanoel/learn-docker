fdfef8464ec9894be1d770dd74b4214ba811be47
Lucas Emanoel <lucas2014.barros@gmail.com>
2023-02-03 15:30:20 +0000

    new: implementation docker using apache-alpine and mysql with phpmyadmin

---
+++ data/php-A/index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+<title>Exemplo Apache</title>
+</head>
+<body>
+<h1> OK !! Apache funcionando!!!!! </h1>
+</body>
+</html>
+
+
+
+
+<?php
+phpinfo();
+?>
+
